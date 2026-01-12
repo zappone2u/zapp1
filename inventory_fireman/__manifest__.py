bb87438fd816ba398267b4a978630de43e333fbc
Zappone Arthur <aza@open-net.ch>
2026-01-12 12:16:25 +0100

    First commit

---
+++ inventory_fireman/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# File: __manifest__.py
+{
+    "name": "Inventory Fireman",
+    "summary": "",
+    "description": "Module to manage inventory for fireman equipment.",
+    "category": "",
+    "author": "ZappOne",
+    "version": "18.0.1.0.0",
+    "website": "https://www.open-net.ch",
+    "license": "OPL-1",
+    "depends": [],
+    "data": [
+        # Data Priority
+        "security/ir.model.access.csv",
+        # Reports
+        # Views
+        # Views Website
+    ],
+    "assets": {},
+    "installable": True,
+    "auto_install": False,
+}
